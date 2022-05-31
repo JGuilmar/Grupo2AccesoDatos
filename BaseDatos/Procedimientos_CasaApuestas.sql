74595d2e8ee3867631b9e935e517c8f1a9bc4b31
Jose Guilmar Bayona Chunga <jose.bayonachunga@plexus.local>
2022-05-31 19:26:29 +0200

    Comentados los procedimientos almacenados de Usuario

--- BaseDatos/Procedimientos_CasaApuestas.sql
+++ BaseDatos/Procedimientos_CasaApuestas.sql
@@ -15,6 +15,7 @@ create procedure registroUsuario
 as
 	declare @codigoError int
 	set @codigoError = 0
+	--Verificamos si el usuario esta registrado
 	if(select count(*) from usuario where nombre = @usuario) = 0
 		insert into USUARIO(nombre, clave, saldo) values (@usuario, @clave, 0)
 	else
@@ -32,6 +33,7 @@ go
 create procedure loginUsuario
 	@usuario varchar(255), @clave varchar(255)
 as
+	--Verificamos si los datos introducidos son correctos.
 	if(select count(*) from USUARIO where @usuario = nombre and @clave = clave) = 1
 		select descripcion from CODIGOS_ERROR where codigo = 0
 	else
@@ -48,11 +50,16 @@ create procedure ingresoSaldo
 	@cantidad int, 
 	@usuarioId  int
 as
+	--Declaramos variables
 	declare @saldoUsuario decimal(10,3), @saldoInicial decimal(10,3), @saldoFinal decimal (10,3)
+	--Extraemos el saldo
 	set @saldoInicial = (Select saldo from USUARIO where @usuarioId = id_usuario)
+		--Ejecución conjunta
 		begin tran
 			set @saldoFinal = @saldoInicial + @cantidad
+			--Actualizamos el saldo del usuario
 			update USUARIO set saldo = @saldoFinal where @usuarioId = id_usuario
+			--Introducimos nueva transaccion
 			Insert into TRANSACCION (id_tipo_transaccion, fecha_transaccion, id_usuario, monto, saldo_inicial, saldo_final)
 			values (1, GETDATE(), @usuarioId, @cantidad, @saldoInicial, @saldoFinal)
 		commit tran
@@ -68,13 +75,18 @@ GO
 create procedure retireSaldo
 	@cantidad int, @usuarioId  int
 as
+	--Declaramos variables
 	declare @saldoUsuario decimal(10,3), @saldoInicial decimal(10,3), @saldoFinal decimal (10,3), @codigoError int
+	--Extraemos el saldo
 	set @saldoInicial = (Select saldo from USUARIO where @usuarioId = id_usuario)
+	--Verificamos que tenemos saldo para retirar
 	if @saldoInicial >= @cantidad
 		begin
 			begin tran
 				set @saldoFinal = @saldoInicial - @cantidad
+				--Actualizamos el saldo del usuario
 				update USUARIO set saldo = @saldoFinal where @usuarioId = id_usuario
+				--Introducimos nueva transaccion
 				Insert into TRANSACCION (id_tipo_transaccion, fecha_transaccion, id_usuario, monto, saldo_inicial, saldo_final)
 				values (2, GETDATE(), @usuarioId, @cantidad, @saldoInicial, @saldoFinal)
 				set @codigoError = 0
@@ -93,20 +105,27 @@ go
 create procedure apostar
 	@usuarioId int, @opcionId int, @cantidad int
 as
+	--Declaramos variables
 	declare @saldoInicial decimal(10,3),@saldoFinal decimal(10,3), @codigoError int, @multiplicador decimal(10,3), @eventoId int, @fechaEvento datetime
+	--Extraemos la fecha del evento a apostar.
 	set @eventoId = (select id_evento from OPCION where @opcionId = id_opcion)
 	set @fechaEvento = (select fecha from EVENTO where @eventoId = id_evento)
+	--Verificamos si el evento aún no se realiza
 	if @fechaEvento > GETDATE()
 		begin
 			set @saldoInicial = (select saldo from USUARIO where @usuarioId = id_usuario)
 			set @multiplicador = (Select multiplicador from OPCION where id_opcion = @opcionId)
+			--Verificamos si tenemos saldo suficiente
 			if @saldoInicial >= @cantidad
 				begin
 					begin tran
+						--Actualizamos el saldo del usuario
 						set @saldoFinal = @saldoInicial - @cantidad
 						update USUARIO set saldo = @saldoFinal where @usuarioId = id_usuario
+						--Introducimos nueva transaccion
 						Insert into TRANSACCION (id_tipo_transaccion, fecha_transaccion, id_usuario, monto, saldo_inicial, saldo_final)
 						values (3, GETDATE(), @usuarioId, @cantidad, @saldoInicial, @saldoFinal)
+						--Introducimos nueva apuesta realizada
 						insert into APUESTA (id_usuario, id_opcion, fecha_apuesta, cantidad, multiplicador, id_transaccionP,id_transaccionC)
 						values (@usuarioId, @opcionId, GETDATE(), @cantidad, @multiplicador, SCOPE_IDENTITY(), null)
 					commit tran
@@ -165,6 +184,7 @@ go
 create procedure verApuestas
 	@usuarioId int
 as
+	--Consultamos y devolvemos datos de las tablas APUESTA y OPCION
 	select cantidad, apuesta.multiplicador, nombre_opcion, ganancia = (cantidad*apuesta.multiplicador), fecha_apuesta
 	from APUESTA LEFT JOIN OPCION on apuesta.id_opcion = opcion.id_opcion
 	where apuesta.id_usuario = @usuarioId
@@ -180,6 +200,7 @@ go
 create procedure verTransacciones
 	@usuarioId int
 as
+	--Consultamos y devolvemos datos de las tablas TRANSACCION y TIPO_TRANSACCION
 	SELECT fecha_transaccion, nombre as 'tipo_transaccion', monto, saldo_inicial, saldo_final 
 	FROM TRANSACCION
 	LEFT JOIN TIPO_TRANSACCION
