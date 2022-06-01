c854bdb18c4a7bef05d4ebc9ac503dbfb6d0fa45
jose.bayonachunga <jose.bayonachunga@PLX10136-010>
2022-06-01 17:23:33 +0200

    Implementacion de los endPoint ingreseDinedo y retireDinero de la Api.

--- API_CasaApuestas/CasaApuestasAPI_csharp/Controllers/UserController.cs
+++ API_CasaApuestas/CasaApuestasAPI_csharp/Controllers/UserController.cs
@@ -12,20 +12,39 @@ namespace CasaApuestasAPI_csharp.Controllers
         BaseDatos conexionBaseDatos = new BaseDatos("localhost", "BetHouse", "bayonaJose", "abc12345..");
 
         [HttpPost("acceder")]
-        public IEnumerable<string> acceso([FromForm] string username, [FromForm]string password)
+        public IEnumerable<string> acceso([FromForm] string usuario, [FromForm]string contraseña)
         {
-            DataTable login = conexionBaseDatos.ejecutarConsulta($"exec loginUsuario '{username}', '{password}'");
+            DataTable login = conexionBaseDatos.ejecutarConsulta($"exec loginUsuario '{usuario}', '{contraseña}'");
 
             return new string[] { login.Rows[0]["descripcion"].ToString() };
         }
 
         [HttpPost("registrar")]
-        public IEnumerable<string> registro([FromForm] string username, [FromForm] string password)
+        public IEnumerable<string> registro([FromForm] string usuario, [FromForm] string contraseña)
         {
-            DataTable registro = conexionBaseDatos.ejecutarConsulta($"exec registroUsuario '{username}', '{password}'");
+            DataTable registro = conexionBaseDatos.ejecutarConsulta($"exec registroUsuario '{usuario}', '{contraseña}'");
 
             return new string[] { registro.Rows[0]["descripcion"].ToString() };
         }
+
+        [HttpPost("ingresarDinero")]
+        public IEnumerable<string> ingresarDinero([FromForm] int usuarioId, [FromForm] int monto)
+        {
+            DataTable ingreso = conexionBaseDatos.ejecutarConsulta($"exec ingresoSaldo '{usuarioId}', '{monto}'");
+
+            return new string[] { $"Usuario: {ingreso.Rows[0]["nombre"].ToString()}", $"Saldo  : {ingreso.Rows[0]["saldo"].ToString()}" };
+        }
+
+        [HttpPost("retirarDinero")]
+        public IEnumerable<string> retirarDinero([FromForm] int usuarioId, [FromForm] int monto)
+        {
+            DataTable retiro = conexionBaseDatos.ejecutarConsulta($"exec retireSaldo '{usuarioId}', '{monto}'");
+
+            return new string[] { retiro.Rows[0]["descripcion"].ToString() };
+        }
+
+
+
 
     }
 }
